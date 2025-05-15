9c6a5ff8595be4794ac5fa1e71c72cb21acd321f
vickygiovi <vickygiovi@gmail.com>
2025-05-15 12:16:20 -0300

    Updated assignment2.sql

--- database/assignment2.sql
+++ database/assignment2.sql
@@ -9,25 +9,25 @@ VALUES (
         'Stark',
         'tony@starkent.com',
         'Iam1ronM@n'
-    )
+    );
 UPDATE public.account
 SET account_type = 'Admin'
 WHERE account_id = 1;
 DELETE FROM public.account
-WHERE account_id = 1
+WHERE account_id = 1;
 UPDATE public.inventory
 SET inv_description = REPLACE(
         inv_description,
         'the small interiors',
         'a huge interior'
     )
-WHERE inv_id = 10
+WHERE inv_id = 10;
 SELECT inv_make,
     inv_model,
     classification_name
 FROM public.inventory AS inv
     INNER JOIN public.classification AS cla ON (inv.classification_id = cla.classification_id)
-WHERE classification_name = 'Sport'
+WHERE classification_name = 'Sport';
 UPDATE public.inventory
 SET inv_image = REPLACE(inv_image, '/images', '/images/vehicles'),
-    inv_thumbnail = REPLACE(inv_thumbnail, '/images', '/images/vehicles')
+    inv_thumbnail = REPLACE(inv_thumbnail, '/images', '/images/vehicles');
